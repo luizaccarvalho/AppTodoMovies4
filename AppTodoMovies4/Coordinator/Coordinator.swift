558e4b16e6578dd8bd3a3d949ae297760274438c
Luiza <luiza.ccarvalho@hotmail.com>
2021-11-12 10:49:00 -0300

    criacao Coordinator
    criacao dos arquivos responsaveis pela navegacao do app

---
+++ AppTodoMovies4/Coordinator/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  AppTodoMovies4
+//
+//  Created by Luiza  Carvalho on 12/11/21.
+//
+
+import Foundation
---
+++ AppTodoMovies4/Coordinator/CoordinatorConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  CoordinatorConfiguration.swift
+//  AppTodoMovies4
+//
+//  Created by Luiza  Carvalho on 12/11/21.
+//
+
+import Foundation
